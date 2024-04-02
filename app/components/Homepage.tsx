4599745a3acb7bfa91dd0d9dbe70acd88bd18d3f
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-04-02 21:11:43 +0530

    changed home page

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,24 +1,31 @@
-"use client";
 import React from "react";
 import Homeimage from "@/public/homepage1.png";
 import Homeeimage from "@/public/hackathon_2_new-removebg-preview.png";
+import Chatbotimage from "@/public/chatbot.jpeg";
 import Image from "next/image";
-import { useTheme } from "next-themes";
+import Link from "next/link";
 
-const homepage = () => {
-  const { theme } = useTheme();
+const Homepage = () => {
   return (
-    <div className=" flex justify-center w-full items-center">
-      <div className={`${theme == "dark" ? "w-[90%]" : "w-[100%]"} `}>
-        <div className="w-full">
-          <Image src={Homeimage} alt="home" className="w-auto h-3/4" />
-        </div>
-        <div className="w-full">
-          <Image src={Homeeimage} alt="home" className="w-auto mt-auto" />
-        </div>
+    <div>
+      <div className="border-white relative border-t">
+        <Image src={Homeimage} alt="home" className="w-full h-90" />
+        <Link
+          href="/create-course"
+          className="p-3 absolute top-1/2 rounded-xl px-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500"
+        >
+          Create course
+        </Link>
+      </div>
+
+      <div className="border-white border-t-2">
+        <Image src={Homeeimage} alt="home" className="w-full h-90 " />
+      </div>
+      <div className="border-white border-t-2">
+        <Image src={Chatbotimage} alt="home" className="w-full h-90 " />
       </div>
     </div>
   );
 };
 
-export default homepage;
+export default Homepage;
